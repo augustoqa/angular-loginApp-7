315944a54f308c9cb9a39e53442ce4b58e78d6fd
César Acual <augustoacual@gmail.com>
2021-03-28 16:58:44 -0600

    Firebase y servicios REST

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -19,6 +20,7 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { UsuarioModel } from '../models/usuario.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private url = 'https://identitytoolkit.googleapis.com/v1';
+  private apiKey = 'AIzaSyD7ktLWpG2IyaYpbLgdGEB6NyClXUdipRE';
+
+
+  // Crearu nuevo usuario
+  // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
+
+  // Login
+  // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+
+  constructor( private http: HttpClient ) { }
+
+  logout() {}
+
+  login( usuario: UsuarioModel ) {}
+
+  nuevoUsuario( usuario: UsuarioModel ) {}
+}
