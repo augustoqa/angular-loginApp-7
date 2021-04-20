9eec75c5f2468d2d6ee4cecb3ceaa5c7a24ba07c
César Acual <augustoacual@gmail.com>
2021-04-20 16:04:07 -0600

    Recordar usuario

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -14,6 +14,7 @@ import Swal from 'sweetalert2';
 export class RegistroComponent implements OnInit {
 
   usuario: UsuarioModel;
+  recordarme: false;
 
   constructor( private auth: AuthService,
                private router: Router ) { }
@@ -36,6 +37,11 @@ export class RegistroComponent implements OnInit {
       .subscribe( resp => {
         Swal.close();
         console.log(resp);
+
+        if (this.recordarme) {
+          localStorage.setItem('email', this.usuario.email);
+        }
+
         this.router.navigateByUrl('/home');
       }, (err) => {
         console.log(err.error.error.message);
